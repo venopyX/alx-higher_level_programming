4f6ae3ac8cea23ae4a3519f18dda1424ae595abb
venopyX <gladsonchala@gmail.com>
2025-01-17 23:04:16 +0300

    add: solution to 0x10-python-network_0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of the response
+# Send a GET request to a given URL with a header variable.
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
