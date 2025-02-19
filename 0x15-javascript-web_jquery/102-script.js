ac7a2a3e88c123961351f0116635258f9ea48404
venopyX <gladsonchala@gmail.com>
2025-02-19 15:45:17 +0300

    feat: script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+// script that fetches and prints how to say “Hello” depending on the language
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    const languageCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
+
+    $.get(apiUrl, function(data) {
+      $('#hello').text(data.hello);
+    }).fail(function() {
+      $('#hello').text("Error fetching translation."); // Handle potential errors
+    });
+  });
+});
