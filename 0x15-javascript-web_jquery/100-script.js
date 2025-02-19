1b98b5d2192c250e36b53d738caf1fc668fcffc7
venopyX <gladsonchala@gmail.com>
2025-02-19 15:41:53 +0300

    feat: script that updates the text color of the <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Script that update header text color using `document.querySelector` (script in `<head>`).
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector('header');
+  if (header) {
+    header.style.color = '#FF0000';
+  }
+});
